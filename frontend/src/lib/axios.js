06619c2180678e9ed8e0210599b36ef7683abc59
tsucare2025-capstone <tsucare2025@gmail.com>
2025-09-05 16:29:47 +0800

    Fix API routes: Add /api prefix to frontend requests

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL || "http://localhost:3000",
+    baseURL: `${process.env.REACT_APP_BACKEND_URL || "http://localhost:3000"}/api`,
     withCredentials: true,
 });
 
